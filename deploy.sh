a3e7dd3bb7d9740ea2d37bedf801c4b337a6355d
mattcleary55 <mattcleary55@gmail.com>
2019-07-30 08:12:19 +0000

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ docker build -t judgejab/multi-client:latest -t judgejab/multi-client:$SHA -f ./
 docker build -t judgejab/multi-server:latest -t judgejab/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t judgejab/multi-worker:latest -t judgejab/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push judgejab/multi-client:latest 
+docker push judgejab/multi-client:latest
 docker push judgejab/multi-server:latest
 docker push judgejab/multi-worker:latest
 
